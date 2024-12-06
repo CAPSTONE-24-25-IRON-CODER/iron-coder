f75c39e8d3327641256e354796d821a533f6f76d
julianbeloiu <julianbeloiu@ufl.edu>
2024-12-05 21:32:29 -0500

    Fixed blink_leds example

--- example-code/blink_leds/src/main.rs
+++ example-code/blink_leds/src/main.rs
@@ -1,92 +1,75 @@
-//! # SparkFun MicroMod Blinky Example
-//!
-//! Blinks the LED on a SparkFun MicroMod RP2040 board.
-//!
-//! This will blink an LED attached to GP25, which is the pin the SparkFun MicroMod RP2040 uses for
-//! the on-board LED.
-//!
-//! See the `Cargo.toml` file for Copyright and license details.
-
 #![no_std]
 #![no_main]
 
-// The macro for our start-up function
-use sparkfun_micromod_rp2040::entry;
+/**** low-level imports *****/
+use core::fmt::Write;
+use core::panic::PanicInfo;
+// use panic_halt as _;
+// use cortex_m::prelude::*;
+use cortex_m_rt::entry;
+use embedded_hal::{
+    digital::v2::{OutputPin},
+};
 
-// GPIO traits
-use embedded_hal::digital::OutputPin;
+/***** board-specific imports *****/
+use adafruit_feather_rp2040::hal as hal;
+use hal::{
+    pac::interrupt,
+    clocks::{init_clocks_and_plls, Clock},
+    pac,
+    watchdog::Watchdog,
+    Sio,
+};
+use adafruit_feather_rp2040::{
+    Pins, XOSC_CRYSTAL_FREQ,
+};
 
-// Ensure we halt the program on panic (if we don't mention this crate it won't
-// be linked)
-use panic_halt as _;
+#[panic_handler]
+fn panic(panic_info: &PanicInfo) -> ! {
+    loop {}
+}
 
-// Pull in any important traits
-use sparkfun_micromod_rp2040::hal::prelude::*;
-
-// A shorter alias for the Peripheral Access Crate, which provides low-level
-// register access
-use sparkfun_micromod_rp2040::hal::pac;
-
-// A shorter alias for the Hardware Abstraction Layer, which provides
-// higher-level drivers.
-use sparkfun_micromod_rp2040::hal;
-
-/// Entry point to our bare-metal application.
-///
-/// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
-/// as soon as all global variables are initialised.
-///
-/// The function configures the RP2040 peripherals, then blinks the LED in an
-/// infinite loop.
 #[entry]
 fn main() -> ! {
-    // Grab our singleton objects
+    // Grab the singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
-
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-
-    // Configure the clocks
-    //
-    // The default is to generate a 125 MHz system clock
-    let clocks = hal::clocks::init_clocks_and_plls(
-        sparkfun_micromod_rp2040::XOSC_CRYSTAL_FREQ,
+    // Init the watchdog timer, to pass into the clock init
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let clocks = init_clocks_and_plls(
+        XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
         pac.PLL_USB,
         &mut pac.RESETS,
         &mut watchdog,
-    )
-        .ok()
-        .unwrap();
+    ).ok().unwrap();
 
-    // The delay object lets us wait for specified amounts of time (in
-    // milliseconds)
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
-    // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
-
-    // Set the pins up according to their function on this particular board
-    let pins = sparkfun_micromod_rp2040::Pins::new(
+    // initialize the Single Cycle IO
+    let sio = Sio::new(pac.SIO);
+    // initialize the pins to default state
+    let pins = Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    // Set the LED to be an output
-    let mut led_pin = pins.led.into_push_pull_output();
+    let mut timer = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut led_pin = pins.d13.into_push_pull_output();
 
-    // Blink the LED at 1 Hz
+    /*
+    Loop Section
+    */
+    let delay: u32 = 500;   // loop delay in ms
+    let mut n: u32 = 0;
     loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
         led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        timer.delay_ms(delay as u32);
+        led_pin.set_high().unwrap();
+        timer.delay_ms(delay as u32);
+        n = n + 1;
     }
-}
 
-// End of file
+}
--- example-code/blink_leds/target/debug/build/heapless-a3869995fe5fb9e9/build_script_build-a3869995fe5fb9e9.d
+++ example-code/blink_leds/target/debug/build/heapless-a3869995fe5fb9e9/build_script_build-a3869995fe5fb9e9.d
@@ -1,5 +1,5 @@
-C:\Users\ethan\Documents\CEN3907\Testing\Testing\target\debug\build\heapless-a3869995fe5fb9e9\build_script_build-a3869995fe5fb9e9.exe: C:\Users\ethan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\heapless-0.8.0\build.rs
+C:\Users\julia\Documents\UF\Design 1\iron-coder\example-code\blink_leds\target\debug\build\heapless-a3869995fe5fb9e9\build_script_build-a3869995fe5fb9e9.exe: C:\Users\julia\.cargo\registry\src\index.crates.io-6f17d22bba15001f\heapless-0.8.0\build.rs
 
-C:\Users\ethan\Documents\CEN3907\Testing\Testing\target\debug\build\heapless-a3869995fe5fb9e9\build_script_build-a3869995fe5fb9e9.d: C:\Users\ethan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\heapless-0.8.0\build.rs
+C:\Users\julia\Documents\UF\Design 1\iron-coder\example-code\blink_leds\target\debug\build\heapless-a3869995fe5fb9e9\build_script_build-a3869995fe5fb9e9.d: C:\Users\julia\.cargo\registry\src\index.crates.io-6f17d22bba15001f\heapless-0.8.0\build.rs
 
-C:\Users\ethan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\heapless-0.8.0\build.rs:
+C:\Users\julia\.cargo\registry\src\index.crates.io-6f17d22bba15001f\heapless-0.8.0\build.rs:
