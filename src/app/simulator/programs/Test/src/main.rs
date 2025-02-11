1d1e24a4a969a0e17d330d8ac307508de2ff01ea
Josue Vicente <josue.vicente@ufl.edu>
2025-02-11 11:02:27 -0500

    Board Updates
    Added logging to the script files and state saving.
    
    Added the skeleton of a new custom board.

--- src/app/simulator/programs/Test/src/main.rs
+++ src/app/simulator/programs/Test/src/main.rs
@@ -1,84 +1,72 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
-use cortex_m_rt::entry;
 use panic_halt as _;
-use stm32f4xx_hal::{pac, prelude::*, serial::{Config, Serial}};
+use cortex_m_rt::entry;
+use stm32f4xx_hal as hal;
+use crate::hal::{block, pac, prelude::*, serial::{Config, Serial}};
+
+use core::fmt::Write;
+use fugit::MicrosDurationU32;
+
+use core::ops::Range;
+
 
 #[entry]
 fn main() -> ! {
+    // Getting device and cortex peripherals 
     let dp = pac::Peripherals::take().unwrap();
-    let gpioa = dp.GPIOA.split();
+    let cp = cortex_m::peripheral::Peripherals::take().unwrap();
 
+    // Splitting GPIOA and GPIOD into separate pins
+    let gpioa = dp.GPIOA.split();
+    let gpiod = dp.GPIOD.split();
+
+    // Configuring Rest and Clock Control, sets up deviced at high speeds
     let rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.use_hse(8.MHz()).freeze();
 
-    let tx_pin = gpioa.pa2.into_alternate();
-    //let rx_pin = gpioa.pa3.into_alternate();
+    // Delay function being initialized
+    let mut delay = cp.SYST.delay(&clocks);
 
-    let mut tx = Serial::tx(
-        dp.USART2,
-        tx_pin,
-        Config::default()
-                  .baudrate(115200.bps())
-                  .wordlength_8()
-                  .parity_none(),
-        &clocks,
+    // define RX/TX pins
+    let tx_pin = gpioa.pa2;
+    let rx_pin = gpioa.pa3;
+
+    // configure serial
+    let serial = dp
+        .USART2
+        .serial(
+            (tx_pin, rx_pin),
+            Config::default().baudrate(9600.bps()).wordlength_8(),
+            &clocks,
         )
-      .unwrap();
+        .unwrap();
+        // if we want differnet size data payload, we need to add additional lines here
+        
+    // split the serial struct into tx and rx and announce the start
+    let (mut tx, mut rx) = serial.split();
+    writeln!(tx, "Getting Started in UART\r").unwrap();
     
-
-    writeln!(tx, "UART Test Initialized\r").unwrap();
-
     loop {
-        writeln!(tx, "USART OUTPUT ENABLED\r").unwrap();
-        cortex_m::asm::delay(8_000_000);
+        let received: u8 = block!(rx.read()).unwrap();
+        let receivedChar = received as char;
+                
+        match receivedChar {
+            '1' => {
+                writeln!(tx, "LED 5 ON").unwrap();
+            }
+            '\r' => {
+                writeln!(tx, "").unwrap();
+            }
+            _=>{
+                writeln!(tx, "Received: {receivedChar:02}").unwrap();
+                // write to a file in the same directory
+                
+            }
+        }
+
+        writeln!(tx, "\r").unwrap();
+        delay.delay(MicrosDurationU32::millis(100));
     }
 }
-
-
-// use stm32f4xx_hal::{
-//     pac,
-//     prelude::*,
-//     serial::{config::Config, Serial, },
-// };
-// use cortex_m_rt::entry;
-// use core::panic::PanicInfo;
-// use panic_halt as _;
-// use fugit::{RateExtU32, ExtU32};
-// use embedded_hal::serial::Write;
-
-// #[entry]
-// fn main() -> ! {
-//     let dp = pac::Peripherals::take().unwrap();
-//     let rcc = dp.RCC.constrain();
-//     let clocks = rcc
-//         .cfgr
-//         .use_hse(8.MHz())
-//         .sysclk(168.MHz())
-//         .pclk1(24.MHz())
-//         .i2s_clk(86.MHz())
-//         .require_pll48clk()
-//         .freeze();
-//         // Test that the I2S clock is suitable for 48000kHz audio.
-
-//     let gpioa = dp.GPIOA.split();
-//     let tx = gpioa.pa9.into_alternate();
-//     let rx = gpioa.pa10.into_alternate();
-
-//     // Configure the UART
-//     let serial = Serial::<_, _, u8>::new( 
-//         dp.USART1,         
-//         (tx, rx),
-//         Config::default().baudrate(9600.bps()),
-//         &clocks,
-//     );
-//     let (mut tx, _rx) = serial.split();
-//     // Send a string through UART
-//     loop {
-//         write_str("Hello, world!").ok();
-//         // Optionally, add a delay here to control the message frequency
-//         cortex_m::asm::delay(8_000_000);
-//     }
-// }
